e3f2a7cf43056e4da115677b9c9ae22f2c202798
igar <buy@arnica-agro.by>
2019-12-09 09:10:45 +0300

    new rep

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import logo from './favicon1.png';
+
+const App = () => {
+    return (
+        <main className="wrapper">
+            <div className="header"><img className="logo" src={logo} alt="logo" /></div>
+            <div className="name"><h1>Автоизация</h1></div>
+            <div className="login">
+                <label>Логин</label>
+                <input type="text" />
+                <label>Пароль</label>
+                <input type="text" />
+                <input type="button" value="ВОЙТИ" />
+            </div>
+        </main>
+    )
+}
+export default App;
